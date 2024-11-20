27326163a0ee4e4a14d8efdb953e51e8a6f70a56
Ahmed_Reda <ahmedreda0120099@gmail.com>
2024-11-20 19:02:09 +0200

    add some data 2

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -5,11 +5,11 @@
       <img
         src="~/assets/img/contact/contact-back.jpg"
         alt="House Image"
-        class="object-cover w-full h-full rounded-lg"
+        class="object-cover w-full h-full rounded-lg br-15"
       />
       <!-- Overlay Text -->
       <div
-        class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-left p-8"
+        class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-left p-8 br-15"
       >
         <div class="text-center">
           <h2 class="text-white text-4xl font-bold">{{ $t("contact-us") }}</h2>
@@ -22,7 +22,7 @@
 
     <!-- Conditional rendering based on form submission -->
     <div
-      class="inset-0 top-48 md:top-60 flex justify-end md:ml-16 contact-form"
+      class="inset-0 top-48 md:top-60 flex justify-end md:ml-16 contact-form pb-8"
     >
       <!-- النموذج أقرب إلى اليسار -->
       <div
@@ -32,48 +32,48 @@
         <!-- تصغير العرض هنا -->
         <form @submit.prevent="handleSubmit" class="space-y-4">
           <div class="flex flex-col">
-            <label for="name" class="text-right mb-2">الاسم ثلاثي</label>
+            <label for="name" class=" mb-2">{{$t('full name')}}</label>
             <input
               type="text"
               id="name"
               class="px-4 py-2 border rounded-md focus:outline-none focus:ring focus:ring-indigo-100"
-              placeholder="أدخل الاسم"
+              v-model="form.full_name"
             />
           </div>
           <div class="flex flex-col">
-            <label for="phone" class="text-right mb-2">رقم الجوال</label>
+            <label for="phone" class=" mb-2">{{$t('mobile number')}}</label>
             <input
               type="text"
               id="phone"
               class="px-4 py-2 border rounded-md focus:outline-none focus:ring focus:ring-indigo-100"
-              placeholder="أدخل رقم الجوال"
+              v-model="form.phone"
             />
           </div>
           <div class="flex flex-col">
-            <label for="email" class="text-right mb-2">البريد الإلكتروني</label>
+            <label for="email" class=" mb-2">{{$t('email')}}</label>
             <input
               type="email"
               id="email"
               class="px-4 py-2 border rounded-md focus:outline-none focus:ring focus:ring-indigo-100"
-              placeholder="أدخل البريد الإلكتروني"
+              v-model="form.email"
             />
           </div>
           <div class="flex flex-col">
-            <label for="message" class="text-right mb-2">نص الرسالة</label>
+            <label for="message" class=" mb-2">{{$t('notes')}}</label>
             <textarea
               id="message"
               class="px-4 py-2 border rounded-md focus:outline-none focus:ring focus:ring-indigo-100"
-              placeholder="أدخل رسالتك"
+              v-model="form.notes"
               rows="4"
             ></textarea>
           </div>
-          <div class="text-right">
+          <div class="">
             <button
               type="submit"
               class="px-6 py-2 text-white rounded-md hover:bg-gray-800 focus:outline-none focus:ring focus:ring-gray-300"
               style="background-color: #264642; color: white"
             >
-              إرسال الطلب
+              {{ $t('submit') }}
             </button>
           </div>
         </form>
@@ -104,12 +104,18 @@ export default {
   data() {
     return {
       isSubmitted: false, // حالة تتبع إذا تم إرسال النموذج
+      form:{
+        full_name:null,
+        email:null,
+        phone:null,
+        notes:null,
+      }
     };
   },
   methods: {
     handleSubmit() {
       // عند إرسال النموذج، نغير حالة isSubmitted إلى true
-      this.isSubmitted = true;
+      // this.isSubmitted = true;
     },
   },
 };
@@ -123,5 +129,8 @@ export default {
   position: sticky;
   margin-top: -5%;
   margin-inline: 3%;
+}
+.back-img{
+  border-radius: 15px;
 }
 </style>
