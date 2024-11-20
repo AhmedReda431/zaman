3ccd01a8c661c465059bc36168050c3d9a0e6fb2
Ahmed_Reda <ahmedreda0120099@gmail.com>
2024-11-20 19:31:44 +0200

    remove body , htlm style from contact us page

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -93,29 +93,45 @@
           alt="Success Image"
           class="mx-auto mb-4"
         /> -->
-        
+
         <div class="success-message-holder">
-        <div class="main-container">
-          <div class="check-container">
-            <div class="check-background">
-              <svg viewBox="0 0 65 51" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M7 25L27.3077 44L58.5 7" stroke="white" stroke-width="13" stroke-linecap="round" stroke-linejoin="round" />
-              </svg>
+          <div class="main-container">
+            <div class="check-container">
+              <div class="check-background">
+                <svg
+                  viewBox="0 0 65 51"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M7 25L27.3077 44L58.5 7"
+                    stroke="white"
+                    stroke-width="13"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </div>
+              <div class="check-shadow"></div>
             </div>
-            <div class="check-shadow"></div>
           </div>
         </div>
-      </div>
         <!-- <h3 class="text-green-600 text-2xl font-bold">
           {{responseMessage}}
         </h3> -->
-        <h3 class="text-green-600 text-2xl font-bold">{{$t('Request sent successfully!')}}</h3>
+        <h3 class="text-green-600 text-2xl font-bold">
+          {{ $t("Request sent successfully!") }}
+        </h3>
         <p class="text-gray-600 mt-2">
-          {{$t('We are happy to communicate with Zaman Company and we will respond as soon as possible')}}
+          {{
+            $t(
+              "We are happy to communicate with Zaman Company and we will respond as soon as possible"
+            )
+          }}
         </p>
       </div>
-      <div class='error' v-if="isSubmitted && errorMessage">
-        {{errorMessage}}
+      <div class="error" v-if="isSubmitted && errorMessage">
+        {{ errorMessage }}
       </div>
     </div>
   </div>
@@ -132,8 +148,8 @@ export default {
         phone: null,
         notes: null,
       },
-      responseMessage:null,
-      errorMessage:null,
+      responseMessage: null,
+      errorMessage: null,
     };
   },
   methods: {
@@ -143,12 +159,12 @@ export default {
       this.$api
         .post("/contact-us")
         .then((res) => {
-          this.responseMessage = res?.data?.message ? res.data.message : null
+          this.responseMessage = res?.data?.message ? res.data.message : null;
           this.isSubmitted = true;
         })
         .catch((err) => {
-          if(err?.response?.data?.message){
-            this.errorMessage = err.response.data.message
+          if (err?.response?.data?.message) {
+            this.errorMessage = err.response.data.message;
           }
         });
     },
@@ -168,127 +184,125 @@ export default {
 .back-img {
   border-radius: 15px;
 }
-.success-message-holder{
+.success-message-holder {
   // HTML font size can define checkbox size
-html,
-body {
-	font-size: 24px;
-}
+  // Sample container
+  .main-container {
+    width: 100%;
+    height: 200px;
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+  }
 
-// Sample container
-.main-container {
-	width: 100%;
-	height: 200px;
-	display: flex;
-	flex-flow: column;
-	justify-content: center;
-	align-items: center;
-}
+  // Main CSS
+  .check-container {
+    width: 6.25rem;
+    height: 7.5rem;
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    justify-content: space-between;
 
-// Main CSS
-.check-container {
-	width: 6.25rem;
-	height: 7.5rem;
-	display: flex;
-	flex-flow: column;
-	align-items: center;
-	justify-content: space-between;
+    .check-background {
+      width: 100%;
+      height: calc(100% - 1.25rem);
+      background: linear-gradient(to bottom right, #5de593, #41d67c);
+      box-shadow: 0px 0px 0px 65px rgba(255, 255, 255, 0.25) inset,
+        0px 0px 0px 65px rgba(255, 255, 255, 0.25) inset;
+      transform: scale(0.84);
+      border-radius: 50%;
+      animation: animateContainer 0.75s ease-out forwards 0.75s;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      opacity: 0;
 
-	.check-background {
-		width: 100%;
-		height: calc(100% - 1.25rem);
-		background: linear-gradient(to bottom right, #5de593, #41d67c);
-		box-shadow: 0px 0px 0px 65px rgba(255, 255, 255, 0.25) inset,
-			0px 0px 0px 65px rgba(255, 255, 255, 0.25) inset;
-		transform: scale(0.84);
-		border-radius: 50%;
-		animation: animateContainer 0.75s ease-out forwards 0.75s;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		opacity: 0;
+      svg {
+        width: 65%;
+        transform: translateY(0.25rem);
+        stroke-dasharray: 80;
+        stroke-dashoffset: 80;
+        animation: animateCheck 0.35s forwards 1.25s ease-out;
+      }
+    }
 
-		svg {
-			width: 65%;
-			transform: translateY(0.25rem);
-			stroke-dasharray: 80;
-			stroke-dashoffset: 80;
-			animation: animateCheck 0.35s forwards 1.25s ease-out;
-		}
-	}
+    .check-shadow {
+      bottom: calc(-15% - 5px);
+      left: 0;
+      border-radius: 50%;
+      background: radial-gradient(
+        closest-side,
+        rgba(73, 218, 131, 1),
+        transparent
+      );
+      animation: animateShadow 0.75s ease-out forwards 0.75s;
+    }
+  }
 
-	.check-shadow {
-		bottom: calc(-15% - 5px);
-		left: 0;
-		border-radius: 50%;
-		background: radial-gradient(closest-side, rgba(73, 218, 131, 1), transparent);
-		animation: animateShadow 0.75s ease-out forwards 0.75s;
-	}
-}
+  @keyframes animateContainer {
+    0% {
+      opacity: 0;
+      transform: scale(0);
+      box-shadow: 0px 0px 0px 65px rgba(255, 255, 255, 0.25) inset,
+        0px 0px 0px 65px rgba(255, 255, 255, 0.25) inset;
+    }
+    25% {
+      opacity: 1;
+      transform: scale(0.9);
+      box-shadow: 0px 0px 0px 65px rgba(255, 255, 255, 0.25) inset,
+        0px 0px 0px 65px rgba(255, 255, 255, 0.25) inset;
+    }
+    43.75% {
+      transform: scale(1.15);
+      box-shadow: 0px 0px 0px 43.334px rgba(255, 255, 255, 0.25) inset,
+        0px 0px 0px 65px rgba(255, 255, 255, 0.25) inset;
+    }
+    62.5% {
+      transform: scale(1);
+      box-shadow: 0px 0px 0px 0px rgba(255, 255, 255, 0.25) inset,
+        0px 0px 0px 21.667px rgba(255, 255, 255, 0.25) inset;
+    }
+    81.25% {
+      box-shadow: 0px 0px 0px 0px rgba(255, 255, 255, 0.25) inset,
+        0px 0px 0px 0px rgba(255, 255, 255, 0.25) inset;
+    }
+    100% {
+      opacity: 1;
+      box-shadow: 0px 0px 0px 0px rgba(255, 255, 255, 0.25) inset,
+        0px 0px 0px 0px rgba(255, 255, 255, 0.25) inset;
+    }
+  }
 
-@keyframes animateContainer {
-	0% {
-		opacity: 0;
-		transform: scale(0);
-		box-shadow: 0px 0px 0px 65px rgba(255, 255, 255, 0.25) inset,
-			0px 0px 0px 65px rgba(255, 255, 255, 0.25) inset;
-	}
-	25% {
-		opacity: 1;
-		transform: scale(0.9);
-		box-shadow: 0px 0px 0px 65px rgba(255, 255, 255, 0.25) inset,
-			0px 0px 0px 65px rgba(255, 255, 255, 0.25) inset;
-	}
-	43.75% {
-		transform: scale(1.15);
-		box-shadow: 0px 0px 0px 43.334px rgba(255, 255, 255, 0.25) inset,
-			0px 0px 0px 65px rgba(255, 255, 255, 0.25) inset;
-	}
-	62.5% {
-		transform: scale(1);
-		box-shadow: 0px 0px 0px 0px rgba(255, 255, 255, 0.25) inset,
-			0px 0px 0px 21.667px rgba(255, 255, 255, 0.25) inset;
-	}
-	81.25% {
-		box-shadow: 0px 0px 0px 0px rgba(255, 255, 255, 0.25) inset,
-			0px 0px 0px 0px rgba(255, 255, 255, 0.25) inset;
-	}
-	100% {
-		opacity: 1;
-		box-shadow: 0px 0px 0px 0px rgba(255, 255, 255, 0.25) inset,
-			0px 0px 0px 0px rgba(255, 255, 255, 0.25) inset;
-	}
-}
+  @keyframes animateCheck {
+    from {
+      stroke-dashoffset: 80;
+    }
+    to {
+      stroke-dashoffset: 0;
+    }
+  }
 
-@keyframes animateCheck {
-	from {
-		stroke-dashoffset: 80;
-	}
-	to {
-		stroke-dashoffset: 0;
-	}
-}
-
-@keyframes animateShadow {
-	0% {
-		opacity: 0;
-		width: 100%;
-		height: 15%;
-	}
-	25% {
-		opacity: 0.25;
-	}
-	43.75% {
-		width: 40%;
-		height: 7%;
-		opacity: 0.35;
-	}
-	100% {
-		width: 85%;
-		height: 15%;
-		opacity: 0.25;
-	}
-}
-
+  @keyframes animateShadow {
+    0% {
+      opacity: 0;
+      width: 100%;
+      height: 15%;
+    }
+    25% {
+      opacity: 0.25;
+    }
+    43.75% {
+      width: 40%;
+      height: 7%;
+      opacity: 0.35;
+    }
+    100% {
+      width: 85%;
+      height: 15%;
+      opacity: 0.25;
+    }
+  }
 }
 </style>
