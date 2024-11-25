221619c6302a2dc8c01bc96c3a480d7adde056d5
Ahmed_Reda <ahmedreda0120099@gmail.com>
2024-11-25 18:49:40 +0200

    add some data 3

--- pages/real-states/[id].vue
+++ pages/real-states/[id].vue
@@ -14,12 +14,17 @@ import {
   RadioGroupDescription,
   RadioGroupLabel,
   RadioGroupOption,
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
 } from "@headlessui/vue";
 
 let propertyInfo = ref([]);
 let streetInfo = ref([]);
 let info = ref([]);
-
+const { $api } = useNuxtApp();
 onMounted(async () => {
   // Ensure this runs only on the client-side
   currentPageUrl = window.location.href;
@@ -29,27 +34,7 @@ onMounted(async () => {
   await fetchRealState(id);
 });
 let currentImageIndex = ref(0);
-let date = "23-8-2024";
-let time = "7:00";
-let propertyCode = "C10304";
-let propertyType = "فيلا للبيع في جدة";
-let neighborhood = "حي النزهة";
-let currentIndex = ref(0);
-let images = [
-  "/api/placeholder/800/600",
-  "/api/placeholder/800/600",
-  "/api/placeholder/800/600",
-];
-let currentTab = ref("images");
-let tabs = ref([
-  { label: "تفاصيل", value: "details" },
-  { label: "صور", value: "images" },
-]);
-let price = "1000000";
-let agentName = "احمد حسن";
-let licenseNumber = "7100017784";
-let contactIcons = [Share2Icon, HeartIcon, MailIcon];
-let actions = ["طلب تمويل عقاري", "حجز معاينة العقار", "الابلاغ عن هذا العقار"];
+
 let currentPageUrl = "";
 let currentPageTitle = "";
 const sharePage = () => {
@@ -80,6 +65,51 @@ const copyToClipboard = (text) => {
   document.execCommand("copy");
   document.body.removeChild(tempInput);
 };
+
+// dialog
+const bookDialogIsOpen = ref(false);
+const reportDialogIsOpen = ref(false);
+
+function closeBookModal() {
+  bookDialogIsOpen.value = false;
+}
+function closeReportModal() {
+  reportDialogIsOpen.value = false;
+}
+function openBookModal() {
+  bookDialogIsOpen.value = true;
+}
+function openReportModal() {
+  reportDialogIsOpen.value = true;
+}
+
+let bookFormData = ref({
+  type: null,
+  note: null,
+  date: null,
+  id: useRoute().params.id,
+});
+const { $toastMessage } = useNuxtApp();
+const showToast = (message, type) => {
+  $toastMessage(message, type); // Use type 'success', 'error', 'info', etc.
+};
+function bookApiCall() {
+  $api
+    .post("/booking-real-estates", bookFormData.value)
+    .then((res) => {
+      console.log("res", res.data.message);
+
+      showToast(res?.data?.message, "success");
+    })
+    .catch((err) => {
+      if (err?.response?.data?.message) {
+        showToast(err?.response?.data?.message, "danger");
+      }
+    })
+    .finally(() => {
+      closeBookModal();
+    });
+}
 </script>
 
 <template>
@@ -101,7 +131,6 @@ const copyToClipboard = (text) => {
         <div class="relative overflow-hidden rounded-lg mb-2 h-96 h-500px">
           <img
             v-if="
-              currentTab == 'images' &&
               realState.images &&
               realState.images.length > 0 &&
               realState.images[currentImageIndex]
@@ -246,8 +275,8 @@ const copyToClipboard = (text) => {
             <div class="bg-white p-3 rounded" v-if="realState?.license_number">
               <p class="text-sm text-gray-600 d-flex gap-2 align-items-center">
                 <span>
-                  <img src="~/assets/Icons/license.svg" alt="email" /> </span
-                >
+                  <img src="~/assets/Icons/license.svg" alt="email" />
+                </span>
                 <span>{{ $t("generalRealEstateLicense") }}</span>
               </p>
               <p class="font-semibold">{{ realState.license_number }}</p>
@@ -255,7 +284,20 @@ const copyToClipboard = (text) => {
           </div>
         </div>
 
-        <!-- Agent info -->
+        <!-- Action buttons -->
+        <div class="p-4 space-y-2 actions-holder">
+          <button
+            class="w-full bg-gray-500 text-white py-2 rounded transition duration-300 ease-in-out"
+            @click="openBookModal"
+          >
+            {{ $t("Book a property inspection") }}
+          </button>
+          <button
+            class="w-full bg-gray-500 text-white py-2 rounded transition duration-300 ease-in-out"
+          >
+            {{ $t("Report this property") }}
+          </button>
+        </div>
       </div>
     </div>
 
@@ -357,10 +399,144 @@ const copyToClipboard = (text) => {
         </div>
       </div>
     </div> -->
+
+    <TransitionRoot appear :show="bookDialogIsOpen" as="template">
+      <Dialog as="div" @close="closeBookModal" class="relative z-10">
+        <TransitionChild
+          as="template"
+          enter="duration-300 ease-out"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="duration-200 ease-in"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
+        >
+          <div class="fixed inset-0 bg-black/25" />
+        </TransitionChild>
+
+        <div class="fixed inset-0 overflow-y-auto">
+          <div
+            class="flex min-h-full items-center justify-center p-4 text-center"
+          >
+            <TransitionChild
+              as="template"
+              enter="duration-300 ease-out"
+              enter-from="opacity-0 scale-95"
+              enter-to="opacity-100 scale-100"
+              leave="duration-200 ease-in"
+              leave-from="opacity-100 scale-100"
+              leave-to="opacity-0 scale-95"
+            >
+              <DialogPanel
+                class="w-full max-w-3xl transform overflow-hidden rounded-2xl bg-white p-0 text-left align-middle shadow-xl transition-all"
+              >
+                <DialogTitle
+                  as="h3"
+                  class="text-lg font-medium leading-6 text-gray-900 bg-main text-white p-4 rounded"
+                >
+                  <div
+                    class="d-flex align-items-center justify-content-between"
+                  >
+                    <div class="title">
+                      {{ $t("Book a property inspection") }}
+                    </div>
+                    <button
+                      class="diismiss-icon border px-3 py-1 rounded"
+                      @click="bookDialogIsOpen = false"
+                    >
+                      x
+                    </button>
+                  </div>
+                </DialogTitle>
+                <div class="mt-2 p-4">
+                  <form>
+                    <div class="grid grid-cols-2 md:grid-cols-2 gap-4 m-4">
+                      <div>
+                        <label class="block text-sm font-medium text-gray-700">
+                          {{ $t("Preview type") }}</label
+                        >
+                        <input
+                          v-model="bookFormData.type"
+                          required
+                          type="text"
+                          class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                        />
+                      </div>
+                      <div>
+                        <label class="block text-sm font-medium text-gray-700">
+                          {{ $t("nspection date") }}</label
+                        >
+                        <input
+                          v-model="bookFormData.date"
+                          required
+                          type="date"
+                          class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                        />
+                      </div>
+                    </div>
+                    <div class="grid grid-cols-1 md:grid-cols-1 gap-4 m-4">
+                      <div>
+                        <label class="block text-sm font-medium text-gray-700">
+                          {{ $t("Preview type") }}</label
+                        >
+                        <textarea
+                          v-model="bookFormData.note"
+                          type="text"
+                          class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                          rows="6"
+                        ></textarea>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+
+                <div class="mt-4 p-4 d-flex justify-content-center mb-3">
+                  <button
+                    @click.stop="bookApiCall"
+                    class="px-6 py-2 text-white rounded-md hover:bg-gray-800 focus:outline-none focus:ring focus:ring-gray-300 d-flex gap-3 py-3"
+                    style="background-color: #264642; color: white"
+                  >
+                    <span>{{ $t("submit") }}</span>
+                    <span class="icon">
+                      <svg
+                        width="23"
+                        height="23"
+                        viewBox="0 0 23 23"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        data-v-f6fc78ba=""
+                      >
+                        <g clip-path="url(#clip0_85_49628)" data-v-f6fc78ba="">
+                          <path
+                            d="M6.54912 15.64L0.504717 12.2912C0.36228 12.1963 0.247336 12.0656 0.171435 11.9121C0.0955337 11.7587 0.0613492 11.588 0.0723167 11.4172C0.076226 11.247 0.127319 11.0811 0.219905 10.9382C0.312492 10.7952 0.442938 10.6808 0.596717 10.6076L21.8579 0.0736193C22.0094 0.0180221 22.1757 0.0180221 22.3271 0.0736193L6.53072 15.7044L6.54912 15.64ZM6.43872 17.0844V22.356C6.43964 22.4497 6.46913 22.5408 6.52323 22.6172C6.57733 22.6937 6.65348 22.7518 6.74149 22.7838C6.8295 22.8158 6.92519 22.8202 7.01575 22.7963C7.10632 22.7725 7.18746 22.7216 7.24832 22.6504L10.9283 18.1608L15.4547 20.7C15.5728 20.778 15.7073 20.8277 15.8477 20.8453C15.9881 20.8628 16.1307 20.8478 16.2643 20.8012C16.4049 20.7536 16.5319 20.6727 16.6344 20.5653C16.7369 20.458 16.812 20.3274 16.8531 20.1848L22.8331 0.920019C22.8382 0.871082 22.8382 0.821756 22.8331 0.772819L6.43872 17.0844Z"
+                            fill="white"
+                            data-v-f6fc78ba=""
+                          ></path>
+                        </g>
+                        <defs data-v-f6fc78ba="">
+                          <clipPath id="clip0_85_49628" data-v-f6fc78ba="">
+                            <rect
+                              width="23"
+                              height="23"
+                              fill="white"
+                              data-v-f6fc78ba=""
+                            ></rect>
+                          </clipPath>
+                        </defs>
+                      </svg>
+                    </span>
+                  </button>
+                </div>
+              </DialogPanel>
+            </TransitionChild>
+          </div>
+        </div>
+      </Dialog>
+    </TransitionRoot>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 /* Add custom styles here if needed */
 
 /* Custom scrollbar styles */
@@ -397,5 +573,12 @@ const copyToClipboard = (text) => {
   scrollbar-width: thin; /* Sets scrollbar width to thin */
   scrollbar-color: #888 #f0f0f0; /* Thumb color and track color */
   border-radius: 10px;
+}
+.actions-holder {
+  button {
+    background-color: #768c88;
+    padding-block: 15px;
+    border-radius: 10px;
+  }
 }
 </style>
